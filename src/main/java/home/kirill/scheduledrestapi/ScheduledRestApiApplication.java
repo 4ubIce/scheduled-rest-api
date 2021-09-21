6af6aeec52789add4809a435c9b1c48e028c884f
Kuprianov Kirill <kuprianov@isida.by>
2021-09-21 14:06:39 +0300

    init commit

---
+++ src/main/java/home/kirill/scheduledrestapi/ScheduledRestApiApplication.java
@@ -0,0 +1,13 @@
+package home.kirill.scheduledrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledRestApiApplication.class, args);
+    }
+
+}
